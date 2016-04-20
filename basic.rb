8ee1e11031cf16c80c3d50673b4150cdea7b7bd6
Kassim Ouattara <ouat.kassim@gmail.com>
2016-04-19 22:08:12 -0400

    print number from 1 to 100

--- basic.rb
+++ basic.rb
@@ -19,16 +19,28 @@ def maxi
    puts num_max
 end
 
-maxi
-	a = [:toyota, :tesla]
-	b = ["Prius", "Model S"]
+
 
 def mix(a, b)
-	
+	a = [:toyota, :tesla]
+	b = ["Prius", "Model S"]
+	h = {}
 	a.length.times do |i|
-		hash[a[i]] = b[i]
+		h[a[i]] = b[i]
 	end
 	return hash[]
 end
 
-puts mix(a,b)
+def print 
+	for i in 1..100
+		if i % 3 == 0 && i % 5 == 0
+			puts "FizzBizz"
+		elsif i % 3 == 0
+			puts "Fizz"
+		elsif i % 5 == 0
+			puts"Buzz"
+		else
+			puts i
+		end								
+	end
+end
